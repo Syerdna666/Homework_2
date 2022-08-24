5e75ceb1c8728204c95e595d870335c6f37db659
Graceless <syerdnaswag@gmail.com>
2022-08-24 16:04:18 +0300

    Added first task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Task_1
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void GetSecondDigit(int number)
+{
+    if ((number >= 100 && number < 1000) | (number <= -100 && number > -1000))
+    {
+        System.Console.WriteLine(Math.Abs((number % 100) / 10));
+    }
+    else
+    {
+        System.Console.WriteLine("Введите трехзначное число");
+    }
+}
+
+GetSecondDigit(832);
+
+
